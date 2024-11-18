62423a031a17bd87c2e8783570e4122f2352b205
Artem Shabunin <Noname2er3@yandex.ru>
2024-11-18 20:10:58 +0300

    add html for other tables, write on sqlalchemy

--- Database.py
+++ Database.py
@@ -5,8 +5,8 @@ def create_database():
     with sl.connect("databaseVSU.db") as database:
         cursor = database.cursor()
 
-        cursor.execute("""DROP TABLE IF EXISTS students""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS students (
+        cursor.execute("""DROP TABLE IF EXISTS Students""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Students (
                         ID_student INTEGER PRIMARY KEY AUTOINCREMENT,
                         ID_kur INTEGER,
                         ID_group INTEGER,
@@ -18,8 +18,8 @@ def create_database():
                         FOREIGN KEY(ID_kur) REFERENCES kurators(ID_kur),
                         FOREIGN KEY(ID_group) REFERENCES groups(ID_group));""")
                         
-        cursor.execute("""DROP TABLE IF EXISTS teachers""")                                        
-        cursor.execute("""CREATE TABLE IF NOT EXISTS teachers (
+        cursor.execute("""DROP TABLE IF EXISTS Teachers""")                                        
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Teachers (
                             ID_teacher INTEGER PRIMARY KEY AUTOINCREMENT,
                             ID_subject INTEGER,
                             name VARCHAR(60) NOT NULL,
@@ -27,13 +27,13 @@ def create_database():
                             email TEXT,
                             FOREIGN KEY(ID_subject) REFERENCES subjects(ID_subject));""")      
 
-        cursor.execute("""DROP TABLE IF EXISTS subjects""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS subjects (
+        cursor.execute("""DROP TABLE IF EXISTS Subjects""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Subjects (
                             ID_subject INTEGER PRIMARY KEY AUTOINCREMENT,
                             title TEXT NOT NULL);""")
                         
-        cursor.execute("""DROP TABLE IF EXISTS kurators""")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS kurators (
+        cursor.execute("""DROP TABLE IF EXISTS Kurators""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Kurators (
                             ID_kur INTEGER PRIMARY KEY AUTOINCREMENT,
                             ID_kurs INTEGER,    
                             name VARCHAR(60) NOT NULL,
@@ -41,8 +41,8 @@ def create_database():
                             email TEXT,
                             FOREIGN KEY(ID_kurs) REFERENCES kurses(ID_kurs));""")   
     
-        cursor.execute("""DROP TABLE IF EXISTS kurses""")                               
-        cursor.execute("""CREATE TABLE IF NOT EXISTS kurses (
+        cursor.execute("""DROP TABLE IF EXISTS Kurses""")                               
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Kurses (
                             ID_kurs INTEGER PRIMARY KEY AUTOINCREMENT,
                             ID_subject INTEGER,
                             ID_teacher INTEGER,
@@ -53,8 +53,8 @@ def create_database():
                             FOREIGN KEY(ID_subject) REFERENCES subjects(ID_subject),
                             FOREIGN KEY(ID_teacher) REFERENCES teachers(ID_teacher));""")        
               
-        cursor.execute("""DROP TABLE IF EXISTS groups""")                    
-        cursor.execute("""CREATE TABLE IF NOT EXISTS groups(
+        cursor.execute("""DROP TABLE IF EXISTS Groups""")                    
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Groups(
                             ID_group INTEGER PRIMARY KEY AUTOINCREMENT, 
                             titleGroup VARCHAR(100) NOT NULL UNIQUE,
                             ID_teacher INTEGER,
@@ -70,27 +70,27 @@ def load_data_from_csv(cursor):
         reader = csv.reader(f)
         header = next(reader)
         insert_values = [(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]) for row in reader]
-    insert_query = "INSERT INTO students (ID_student, ID_kur, ID_group, name, surname, age, email, passport) VALUES (?,?,?,?,?,?,?,?)"  
+    insert_query = "INSERT INTO Students (ID_student, ID_kur, ID_group, name, surname, age, email, passport) VALUES (?,?,?,?,?,?,?,?)"  
     cursor.executemany(insert_query, insert_values)
 
     with open("subjects_data.csv", newline='', encoding='utf-8') as f1:
         reader = csv.reader(f1)
         header = next(reader)
         insert_values = [(row[0], row[1]) for row in reader]
-    insert_query_subjects = "INSERT INTO subjects (ID_subject, title) VALUES (?,?)"
+    insert_query_subjects = "INSERT INTO Subjects (ID_subject, title) VALUES (?,?)"
     cursor.executemany(insert_query_subjects, insert_values)
 
     with open("teachers_data.csv", newline='', encoding='utf-8') as f2:
         reader = csv.reader(f2)
         header = next(reader)
         insert_values = [(row[0], row[1], row[2], row[3], row[4]) for row in reader]
-    insert_query_teachers = "INSERT INTO teachers (ID_teacher, ID_subject, name, surname, email) VALUES (?,?,?,?,?)"  
+    insert_query_teachers = "INSERT INTO Teachers (ID_teacher, ID_subject, name, surname, email) VALUES (?,?,?,?,?)"  
     cursor.executemany(insert_query_teachers, insert_values)
 
     with open("groups_data.csv", newline='', encoding='utf-8') as f3:
         reader = csv.reader(f3)
         header = next(reader)
         insert_values = [(row[0], row[1], row[2]) for row in reader]
-    insert_query_groups = "INSERT INTO groups (ID_group, titleGroup, ID_teacher) VALUES (?,?,?)"  
+    insert_query_groups = "INSERT INTO Groups (ID_group, titleGroup, ID_teacher) VALUES (?,?,?)"  
     cursor.executemany(insert_query_groups, insert_values)
 
